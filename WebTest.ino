f893cb86d2bcd354dbb22efdad973f302c039395
William Dunkley <wdunkley@zaxisinc.com>
2024-08-15 16:49:24 -0600

    NO

---
+++ WebTest.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+}
+
+void loop() {
+  Serial.println("HI BITCH");
+  delay(2000);
+  // put your main code here, to run repeatedly:
+
+}
